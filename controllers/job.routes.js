26f80b6ace1795dde1c65ae4bdd8d534afb803a1
sarahlahdan-dot <sarah.lahdan@gmail.com>
2026-05-13 18:14:51 +0300

    routes

--- controllers/job.routes.js
+++ controllers/job.routes.js
@@ -1,7 +1,66 @@
 const router = require('express').Router()
 const Job = require('../models/Job');
 const verifyToken = require('../middleware/verify-token');
+const Application = require('../models/Application');
 
 
+// Get all jobs
+router.get('/', verifyToken, async (req, res) => {
+    try {
+        const allJobs = await Job.find()
+            .populate({
+                path: 'postedBy',
+                select: 'title company'
+            });
+
+        res.status(200).json({ jobs: allJobs });
+    } catch (error) {
+        res.status(500).json({ message: error.message || 'Failed to fetch jobs' });
+    }
+})
+
+//CREATE A NEW JOB 
+router.post('/', verifyToken, async (req, res) => {
+  try {
+
+    if (req.user.role !== 'employer') {
+      return res.status(403).json({
+        err: 'Job posting is only allowed for employers !'
+      })
+    }
+
+     req.body.postedBy = req.user._id
+
+    const createJob = await Job.create(req.body)
+    res.status(201).json(createJob)
+  } 
+
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+//GET A JOB BY ID
+router.get(':/id', verifyToken, async(req, res)=>{
+    try{
+        const getJob = await Job.findById(req.params.id).populate('postedBy','username')
+        if (!getJob) return res.status(404).json({ err: 'Job not found'})
+         
+        const user = await User.findById(req.user._id)
+        const { matchScore, matchedSkills, matchedSkills} = calculateMatch(user, getJob)
+        
+        const applyAlready = await Application.findOne({
+            job: req.params.id,
+            applicant: req.user._id
+        })
+        res.json({...getJob.toObject(),matchScore,matchedSkills, missingSkills, applyAlready: !!applyAlready
+        })
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
