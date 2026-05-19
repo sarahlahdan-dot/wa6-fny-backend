9403a4d4f5bcd4a0da97993dd0338fc21cb2749d
sarahlahdan-dot <sarah.lahdan@gmail.com>
2026-05-19 14:10:01 +0300

    backend fixes

--- constants/skills.js
+++ constants/skills.js
@@ -1,52 +1,28 @@
-module.exports = [
+const skillsByCategory = {
+  'Tech': [
+    'JavaScript', 'React', 'Node.js', 'Express', 'MongoDB', 'Python', 'Django', 'SQL', 'Java', 'HTML', 'CSS', '.NET', 'Git', 'AWS'
+  ],
+  'Design': [
+    'UI/UX', 'Graphic Design', 'Figma', 'Adobe Photoshop', 'Video Editing'
+  ],
+  'Business': [
+    'Microsoft Excel', 'Microsoft Word', 'Project Management', 'Data Entry', 'Accounting', 'Bookkeeping'
+  ],
+  'Marketing': [
+    'Digital Marketing', 'Social Media Management', 'Sales', 'Customer Service', 'SEO'
+  ],
+  'Communication': [
+    'Public Speaking', 'Teamwork', 'Leadership', 'Communication'
+  ],
+  'Education': [
+    'Teaching', 'Tutoring'
+  ],
+  'Languages': [
+    'Arabic', 'English', 'French', 'Spanish', 'German', 'Chinese', 'Japanese'
+  ]
+};
 
-  // Programming & Tech
-  'JavaScript',
-  'React',
-  'Node.js',
-  'Express',
-  'MongoDB',
-  'Python',
-  'Java',
-  'SQL',
-  'HTML',
-  'CSS',
-  'Git',
-  'AWS',
 
-  // Design
-  'UI/UX',
-  'Graphic Design',
-  'Figma',
-  'Adobe Photoshop',
-  'Video Editing',
+module.exports = skillsList;
 
-  // Business & Office
-  'Microsoft Excel',
-  'Microsoft Word',
-  'Project Management',
-  'Data Entry',
-  'Accounting',
-  'Bookkeeping',
-
-  // Marketing & Sales
-  'Digital Marketing',
-  'Social Media Management',
-  'Sales',
-  'Customer Service',
-  'SEO',
-
-  // Communication
-  'Public Speaking',
-  'Teamwork',
-  'Leadership',
-  'Communication',
-
-  // Education
-  'Teaching',
-  'Tutoring',
-
-  // Languages
-  'Arabic',
-  'English'
-];
+module.exports = skillsByCategory;
